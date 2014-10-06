06d86b8b240b52544c2e3ec8bde28af470ecbce9
Ruzzz <ruzzzua@gmail.com>
2014-10-06 22:06:37 +0300

    del test

--- test/vs2013_LzmaLibTest/vs_clean_folders.bat
+++
@@ -1,9 +0,0 @@
-call :CLEANUP
-goto :EOF
-
-:CLEANUP
-del /F /Q *.ncb *.plog *.pdb *.lib *.ilk *.exp *.*.user *.obj *.sdf *.aps
-del /F /Q /A:H  *.suo *.opensdf
-rmdir /S /Q ipch
-rmdir /S /Q Debug
-rmdir /S /Q Release
